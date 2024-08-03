95f23a28a69187a65ff8b971286b41cb4717d51c
Anish Sahoo <anishsahoo2005@gmail.com>
2024-08-02 21:08:08 -0400

    Add json and logging

--- build.sbt
+++ build.sbt
@@ -13,7 +13,9 @@ val AkkaVersion = "2.9.3"
 val AkkaHttpVersion = "10.6.3"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
-  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
+  "io.spray" %% "spray-json" % "1.3.6",
+  "ch.qos.logback" % "logback-classic" % "1.5.6"
 )
-
-libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.5.6"
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -5,26 +5,41 @@ import akka.http.scaladsl.server.Directives.*
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
+import spray.json.DefaultJsonProtocol.*
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
+import akka.http.scaladsl.model.StatusCodes
+import org.slf4j.LoggerFactory
+import spray.json.RootJsonFormat
+
+case class Message(message: String)
 
 @main def main(): Unit = {
 
   implicit val system: ActorSystem[Any] = ActorSystem(Behaviors.empty, "ChatServer")
   implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
+  implicit val messageFormat: RootJsonFormat[Message] = jsonFormat1(Message.apply)
+  val logger = LoggerFactory.getLogger("ChatServerLogger")
+
   val route =
     path("") {
       get {
+        logger.info("GET /")
         complete(
-          "Hello, World!"
+          StatusCodes.OK,
+          Message.apply("Hello, World!"),
         )
       }
     }
 
   val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
-  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  logger.info(s"Server online at http://localhost:8080/")
+  logger.debug("Press RETURN to stop...")
 
   StdIn.readLine()
   bindingFuture
     .flatMap(_.unbind())
-    .onComplete(_ => system.terminate())
+    .onComplete(_ =>
+      logger.info("Server stopped")
+      system.terminate())
 }
