8578acf23ab4435789e17f12f23cde56ef805552
Anish Sahoo <anishsahoo2005@gmail.com>
2024-08-02 22:24:45 -0400

    added some files, scala is weird

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,15 +1,15 @@
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives.*
+import org.slf4j.LoggerFactory
+import spray.json.DefaultJsonProtocol.*
+import spray.json.RootJsonFormat
 
 import scala.concurrent.ExecutionContextExecutor
-import scala.io.{Source, StdIn}
-import spray.json.DefaultJsonProtocol.*
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
-import org.slf4j.LoggerFactory
-import spray.json.RootJsonFormat
+import scala.io.StdIn
 
 case class Message(message: String)
 
@@ -22,12 +22,9 @@ case class Message(message: String)
   val logger = LoggerFactory.getLogger("ChatServerLogger")
 
   val routes =
-    path("") {
-      get {
-        logger.info("GET /")
-        val fileContent = Source.fromResource("public/index.html").mkString
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, fileContent))
-      }
+    pathSingleSlash {
+      logger.info("GET /")
+      getFromResource("public/index.html")
     } ~
     path("send-message") {
       post {
@@ -38,8 +35,8 @@ case class Message(message: String)
       }
     }
 
-  val bindingFuture = Http().newServerAt("localhost", 8080).bind(routes)
-  logger.info(s"Server online at http://localhost:8080/")
+  val bindingFuture = Http().newServerAt("localhost", 7999).bind(routes)
+  logger.info(s"Server online at http://localhost:7999/")
   logger.debug("Press RETURN to stop...")
 
   StdIn.readLine()
